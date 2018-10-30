a48fc398c2720085fd2cdfb36b760940014c4bdb
martyup <martine.o.j@gmail.com>
2018-10-30 07:37:01 +0100

    bytter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,70 +1,61 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Camera } from '@ionic-native/camera';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { IonicStorageModule, Storage } from '@ionic/storage';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
-import { Items } from '../mocks/providers/items';
-import { Settings, User, Api } from '../providers';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-// The translate loader needs to know where to load i18n files
-// in Ionic's static asset pipeline.
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+import { ProfilePage } from '../pages/profile/profile';
+import { InfoPage } from '../pages/info/info';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TreningPage } from '../pages/trening/trening';
+import { MatPage } from '../pages/mat/mat';
+import { HelsePage } from '../pages/helse/helse';
+import { KostPage } from '../pages/kost/kost';
+import { KalenderPage } from '../pages/kalender/kalender';
+import { FaqPage } from '../pages/faq/faq';
 
-export function provideSettings(storage: Storage) {
-  /**
-   * The Settings provider takes a set of default settings for your app.
-   *
-   * You can add new settings options at any time. Once the settings are saved,
-   * these values will not overwrite the saved values (this can be done manually if desired).
-   */
-  return new Settings(storage, {
-    option1: true,
-    option2: 'Ionitron J. Framework',
-    option3: '3',
-    option4: 'Hello'
-  });
-}
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Calendar } from '@ionic-native/calendar';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    ProfilePage,
+    InfoPage,
+    HomePage,
+    TabsPage,
+    TreningPage,
+    MatPage,
+    HelsePage,
+    KostPage,
+    KalenderPage,
+    FaqPage
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    IonicModule.forRoot(MyApp),
-    IonicStorageModule.forRoot()
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    ProfilePage,
+    InfoPage,
+    HomePage,
+    TabsPage,
+    TreningPage,
+    MatPage,
+    HelsePage,
+    KostPage,
+    KalenderPage,
+    FaqPage
   ],
   providers: [
-    Api,
-    Items,
-    User,
-    Camera,
-    SplashScreen,
     StatusBar,
-    { provide: Settings, useFactory: provideSettings, deps: [Storage] },
-    // Keep this to enable Ionic's runtime error handling during development
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    Calendar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule { }
+export class AppModule {}
